3f017b5e4d918e7c8dd1d5b19c5f7db342d869bc
NithinKumar121 <nithinkumar678555@gmail.com>
2024-03-13 12:09:20 +0530

    completed signup login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -79,9 +79,21 @@ function App() {
     }
 ]
   return (
-    <>
-      <Signup/>
-    </>
+    <div className='flex  items-center flex-col w-[100%] h-[100%]'>
+    <NavBar/>
+    <div className='bg-[#ffffff] flex  flex-wrap h-[100%] w-[80%] border-2 shadow-xl mt-5 mb-10 p-2'>
+      {dummyData.map((val,index)=>{
+        return  <Link to='/Rent'> <div className='flex mt-10'>
+        <div className='w-10 '></div>
+        <CarDetailCard key={index} name={val.name}  image={val.image} price={val.price} location={val.location}/>
+        </div> 
+        
+        </Link>
+       
+      })
+      }
+    </div>
+  </div>
   )
 }
 
