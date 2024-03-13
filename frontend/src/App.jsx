fc25bd599d73a53080ba3f439064376eb2f7ccfc
NithinKumar121 <nithinkumar678555@gmail.com>
2024-03-13 11:56:40 +0530

    created login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './Components/LoginPage.css'
+import Login from './Components/LoginPage'
+import Signup from './Components/SignupPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Signup/>
     </>
   )
 }
